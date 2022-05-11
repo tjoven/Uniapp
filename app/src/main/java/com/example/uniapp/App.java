8d98eef55afc06450ddcbb5e9aaf53ee7b245686
tianzw <tianzw@lenovocloud.com>
2022-05-11 16:02:47 +0800

    uniapp扩展原生能力

--- app/src/main/java/com/example/uniapp/App.java
+++ app/src/main/java/com/example/uniapp/App.java
@@ -1,17 +1,16 @@
 package com.example.uniapp;
 
 import android.app.Application;
-//import com.example.testmodule.TestModule;
+
+import com.example.testmodule.TestModule;
 import com.taobao.weex.WXSDKEngine;
-import com.taobao.weex.common.WXException;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         try {
-//            WXSDKEngine.registerModule("TestModule", TestModule.class);
-//            WXSDKEngine.registerComponent("myText", TestText.class);
+            WXSDKEngine.registerModule("TestModule", TestModule.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
